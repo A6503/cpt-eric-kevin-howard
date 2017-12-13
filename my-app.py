1532538ad0b13fdf8b5a99ec5a8d7ed97083d1c1
K Li <33941258+A6503@users.noreply.github.com>
2017-12-13 13:12:08 -0500

    Update my-app.py

--- my-app.py
+++ my-app.py
@@ -1,7 +1,82 @@
 # Template for Processing sketches.
 
-def setup():
-  size(400, 400)
-  
- def draw():
-  background(255)
+ def setup():
+    size(1000, 500)
+    
+tank = PVector(400, 400)
+turn = 0
+speed = 0
+#Moving tank
+moveUp = False
+moveBack = False
+turnCCW = False
+turnCW = False
+def draw():
+    global speed
+    global turn
+    global tank
+    #Move tank
+    global moveUp
+    global moveBack
+    global turnCCW
+    global turnCW
+    background(255)
+    fill(0, 255, 0)
+   
+    translate(tank.x, tank.y)
+    rotate(radians(turn))
+    fill(0, 255, 0)
+    rect(-20, -20, 40, 40)
+    rect(-20, -15, 40, 30)
+    rect(0, -5, 30, 10)
+   
+    fill(0, 255, 0)
+    ellipse(0, 0, 25, 25)
+    speed = PVector.fromAngle(radians(turn))
+    speed.mult(3)
+    if tank.x >= 1000 :
+        tank.x = 999
+        turn += 180
+    elif tank.x <= 0:
+        tank.x = 1
+        turn += 180
+    elif moveUp == True:
+        tank.add(speed)
+    elif moveBack == True:
+        tank.sub(speed)
+    if tank.y >= 500 or tank.y <= 0:
+        turn *= -1
+    elif turnCCW == True:
+        turn -= 4
+    elif turnCW == True:
+        turn += 4
+def keyPressed():
+    global moveUp
+    global moveBack
+    global turnCCW
+    global turnCW
+    if key == "w":
+        moveUp = True
+        moveBack = False
+    if key == "s":
+        moveBack = True
+        moveUp = False
+    if key == "a":
+        turnCCW = True
+        turnCW = False
+    if key == "d":
+        turnCW = True
+        turnCCW = False
+def keyReleased():
+    global moveUp
+    global moveBack
+    global turnCCW
+    global turnCW
+    if key == "w":
+        moveUp = False
+    if key == "s":
+        moveBack = False
+    if key == "a":
+        turnCCW = False
+    if key == "d":
+        turnCW = False
